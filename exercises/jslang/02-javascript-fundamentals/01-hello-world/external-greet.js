0fed12bad5a58aa1e34d0a150760dbcaedf4a242
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2026-02-09 09:29:11 +0530

    First gen (holy shit!)

---
+++ exercises/jslang/02-javascript-fundamentals/01-hello-world/external-greet.js
@@ -0,0 +1,8 @@
+// This file is loaded by the step 2 exercises via <script src="...">.
+// It defines a function that can be called from later script blocks.
+
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+window.__externalGreetLoaded = true;
---
+++ exercises/jslang/02-javascript-fundamentals/01-hello-world/external-hello.js
@@ -0,0 +1,5 @@
+// This file is loaded by the step 2 exercises via <script src="...">.
+console.log("external-hello.js loaded");
+
+// Set a global variable so the exercises can verify this ran
+window.__externalHelloLoaded = true;
